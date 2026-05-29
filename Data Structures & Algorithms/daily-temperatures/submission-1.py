4a56547831867c555ec21e8eea16b0df32dddacd
Sahil Patel <50974651+spatel475@users.noreply.github.com>
2026-05-28 23:05:01 -0500

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        output = [0]*len(temperatures)
+
+        for left in range(len(temperatures)):
+            right = left
+
+            for right in range(left, len(temperatures)):
+                if temperatures[right] > temperatures[left]:
+                    output[left] = right-left
+                    break
+                    print(output, left, right)
+
+        return output
